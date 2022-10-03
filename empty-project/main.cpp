f659dccc58c24e66e5a34a8b261e5a0ddb2d78c7
Shea Williams <sheapwilliams@gmail.com>
2022-10-03 11:57:22 -0600

    add empty project files

---
+++ empty-project/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv) {
+  std::cout << "Sample program" << std::endl;
+  return 0;
+}
---
+++ empty-project/unit/test.cpp
@@ -0,0 +1,9 @@
+#include <GUnit/GAssert.h>
+#include <GUnit/GTest.h>
+
+#include <vector>
+
+GTEST("test stuff") {
+  SHOULD("test test") {
+  }
+}
