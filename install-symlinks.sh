08c278e18df69ef8985f8769cecf976e70bcb632
Shea Williams <sheapwilliams@gmail.com>
2022-11-07 15:30:55 -0700

    expect to run in repo

--- install-symlinks.sh
+++ install-symlinks.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ln -s "${HOME}/dotfiles/.bashrc" "${HOME}/.bashrc"
-ln -s "${HOME}/dotfiles/.bash_profile" ${HOME}/.bash_profile
-ln -s "${HOME}/dotfiles/vim" .vim
-ln -s "${HOME}/dotfiles/.vimrc" .vimrc
+ln -s ".bashrc" "${HOME}/.bashrc"
+ln -s ".bash_profile" ${HOME}/.bash_profile
+ln -s "vim" .vim
+ln -s ".vimrc" .vimrc
 
-LOCAL_PROFILE=${HOME}/dotfiles/.bash_profile_linux
+LOCAL_PROFILE=.bash_profile_linux
 if [ $(uname -s) == "Darwin" ] ; then
   LOCAL_PROFILE=.bash_profile_mac
 fi
