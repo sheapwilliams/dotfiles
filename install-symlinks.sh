6e045c31bec3db74be8cee92a3c1cd0cfa917b59
Shea Williams <sheapwilliams@gmail.com>
2022-11-07 16:29:06 -0700

    fixup install script

--- install-symlinks.sh
+++ install-symlinks.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-ln -s ".bashrc" "${HOME}/.bashrc"
-ln -s ".bash_profile" ${HOME}/.bash_profile
-ln -s "vim" .vim
-ln -s ".vimrc" .vimrc
+ln -s "${PWD}/.bashrc" "${HOME}/.bashrc"
+ln -s "${PWD}/.bash_profile" ${HOME}/.bash_profile
+ln -s "${PWD}/vim" "${HOME}/.vim"
+ln -s "${PWD}/.vimrc" "${HOME}/.vimrc"
+ln -s "${PWD}/.gitconfig" "${HOME}/.gitconfig"
 
 LOCAL_PROFILE=.bash_profile_linux
 if [ $(uname -s) == "Darwin" ] ; then
   LOCAL_PROFILE=.bash_profile_mac
 fi
 
-ln -s ${LOCAL_PROFILE} ${HOME}/.bash_profile
+ln -s ${LOCAL_PROFILE} ${HOME}/.bash_profile_local
