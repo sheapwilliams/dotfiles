db91175c37afd6ff1fa8ee3d1e40341c900629b9
Shea Williams <sheapwilliams@gmail.com>
2022-12-14 22:32:53 -0700

    install tmux config

--- install-symlinks.sh
+++ install-symlinks.sh
@@ -5,6 +5,7 @@ ln -s "${PWD}/.bash_profile" ${HOME}/.bash_profile
 ln -s "${PWD}/vim" "${HOME}/.vim"
 ln -s "${PWD}/.vimrc" "${HOME}/.vimrc"
 ln -s "${PWD}/.gitconfig" "${HOME}/.gitconfig"
+ln -s "${PWD}/.tmux.conf" "${HOME}/.tmux.conf"
 
 LOCAL_PROFILE=.bash_profile_linux
 if [ $(uname -s) == "Darwin" ] ; then
